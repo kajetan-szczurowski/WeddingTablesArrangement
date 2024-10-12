53b0304ff930d5c231cf29fbc3c596b5f323ad3f
Kajos <kajetan.szczurowski@gmail.com>
2024-10-12 23:04:33 +0200

    separated files: script for functions library and app for executable instructions

---
+++ app.js
@@ -0,0 +1,43 @@
+//tablesSourceData type = {label:string, data: {'1':'value_1', '2':'value_2', ..., 'n':'value_n'} }[]
+const ADD_INDEX_TO_PERSON_NAME = true;
+const wrapper = document.getElementById('main-wrapper');
+const listContainer = document.querySelector('.alphabetical-list');
+
+insertTables();
+insertAlphabeticalLists();
+
+
+function insertTables(){
+    tablesSourceData.forEach(tableData => {
+        const newTable = createTable(tableData.label, tableData.data, ADD_INDEX_TO_PERSON_NAME);
+        wrapper.appendChild(newTable);
+      })
+}
+
+function insertAlphabeticalLists(countByNames = true){
+    const peopleWithoutTableName = tablesSourceData.map(source => source.data);
+    const alphabeticalList = [];
+    const all = [];
+    peopleWithoutTableName.forEach(oneTable => {
+        const keys = Object.keys(oneTable);
+        const results = keys.map(key => {all.push(oneTable[key]); return reverseFullName(oneTable[key])});
+        alphabeticalList.push(results.sort());
+    })
+      
+      
+    alphabeticalList.forEach(sortedTable => {
+        const newWrapper = document.createElement('ul');
+        const items = sortedTable.map(person => getListItemWithText(fullNameArrayIntoNiceString(person)));
+        newWrapper.append(...items);
+        listContainer.appendChild(newWrapper);
+    })
+
+    if (countByNames) countPeopleByNames(all);
+}
+
+function countPeopleByNames(allGueastList){
+    const countedPeople = {};
+    allGueastList.forEach(person =>{const name = getFirstName(person); (countedPeople[name]? countedPeople[name]++: countedPeople[name] = 1)});
+    const countedAndSortedPeople = Object.keys(countedPeople).map(key => {return {value: countedPeople[key], label: key}}).sort((a, b) => b.value - a.value);
+    console.log(countedAndSortedPeople)
+}
--- script.js
+++ script.js
@@ -1,36 +1,8 @@
-//tablesSourceData type = {label:string, data: {'1':'value_1', '2':'value_2', ..., 'n':'value_n'} }[]
-const wrapper = document.getElementById('main-wrapper');
-const listContainer = document.querySelector('.alphabetical-list');
-
-tablesSourceData.forEach(tableData => {
-  const newTable = createTable(tableData.label, tableData.data);
-  wrapper.appendChild(newTable);
-})
-
-const peopleWithoutTableName = tablesSourceData.map(source => source.data);
-const alphabeticalList = [];
-const all = [];
-peopleWithoutTableName.forEach(oneTable => {
-    const keys = Object.keys(oneTable);
-    const results = keys.map(key => {all.push(oneTable[key]); return reverseFullName(oneTable[key])});
-    alphabeticalList.push(results.sort());
-})
-
-
-alphabeticalList.forEach(sortedTable => {
-  const newWrapper = document.createElement('ul');
-  const items = sortedTable.map(person => getListItemWithText(fullNameArrayIntoNiceString(person)));
-  newWrapper.append(...items);
-  listContainer.appendChild(newWrapper);
-})
-
-
-
-
-
-function createTable(label, people){
+function createTable(label, people, personWithIndex = true){
   const newTable = createTableWrapper(label);
-  const tableArray = Object.keys(people).map(id => `${id}. ${people[id]}`);
+  const tableArray = personWithIndex?
+     Object.keys(people).map(id => `${id}. ${people[id]}`):
+     Object.keys(people).map(id => `${people[id]}`);
 
   let index = 0;
   tableArray.forEach(person =>{
@@ -98,4 +70,8 @@ function fullNameArrayIntoNiceString(fullNameArray){
 function reverseFullName(nameAndSurname){
     const separated = nameAndSurname.split(" ");
     return [separated[1], separated[0]];
+}
+
+function getFirstName(fullName){
+  return fullName.substring(0, fullName.indexOf(" "));
 }
